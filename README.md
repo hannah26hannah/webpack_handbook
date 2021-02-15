a6c5c4173dee76641aa7ece214bb53415af25205
hannah26hannah <ooyoonoo24@gmail.com>
2021-02-15 21:36:54 +0900

    :wrench: experience configuration options for bundling

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # webpack tutorial
 - Code Splitting
+- Properties (Entry, Output, Loader, Plugin)
+- Webpack-dev-server
+- Hot Module Replacement
+
+# Reference
+[Webpack Handbook](https://joshua1988.github.io/webpack-guide/)
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,6 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 const path = require("path");
 
 module.exports = {
@@ -6,5 +9,38 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: "/node_modules",
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    "css-loader"
+                ]
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, "./dist/"),
+        port: 9001,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css'
+        }),
+        new HtmlWebPackPlugin({
+            minify: {
+                collapseWhitespace: true
+            },
+            hash: true,
+            template: './index.html',
+            filename: 'index.html'
+        })
+    ],
+    resolve: {
+        modules: ['node_modules'],
+        extensions: ['.js', '.json', '.jsx', '.css']
     }
 }
